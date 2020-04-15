6b4d5df4b9d0000015ea5d45770c6b9c6f8ee96e
Shraddha Singh <shraddhasingh@QAIT.COM>
2020-04-15 15:39:07 +0530

    Basic frameowrk structore

---
+++ src/test/java/com/contentTest/Features/ResourcesAndMenu.feature
@@ -0,0 +1,25 @@
+Feature: Launchpad Login
+Scenario: Login into Launchpad as Instructor
+	Given User is already on login page
+	When User enters "G.s.instructor@macmillan.com" and "Password@123"
+	And User selects SignIn button    
+	Then User is logged in as Instructor
+	And User selects the course name
+		
+Scenario: Verify the TOC of the course
+    Given User is on the course page
+    When User selects any unit 
+    Then All the chapters should appear in content
+    
+Scenario: Verify the Resources under Content By Type
+    Given User has selected Resources tab
+    When User selects Content By Type
+    Then All the resources should appear as in menuscript
+    
+    
+    
+    
+ 	
+	
+	
+	
---
+++ src/test/java/com/contentTest/StepDefinitions/ResourcesStepDef.java
@@ -0,0 +1,161 @@
+package com.contentTest.StepDefinitions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.junit.Assert;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.testng.annotations.Test;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
+
+
+
+public class ResourcesStepDef {
+	
+	WebDriver wd;
+	
+	
+	@Given ("^User is already on login page$")
+	public void user_is_already_on_login_page()
+	{
+	   
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+		wd= new ChromeDriver();
+		wd.get("http://www.highschool.bfwpub.com/launchpad/spa3edemo");
+		
+		
+	}
+	
+	@When("^User enters \\”(.*)\\” and \\”(.*)\\”$")
+	public void user_enters_username_and_password(String usr, String pwd)
+	{
+	
+		
+	}
+	
+	@And("^User selects SignIn button$")
+	public void User_selects_SignIn_button()
+	{
+		
+	}
+	
+	@And("^User selects the course name$")
+	public void user_selects_the_course_name(String CourseName)
+	{
+		wd.findElement(By.partialLinkText("Starnes, Statistics and Probability with Applications 3e Demo LaunchPad")).click();
+		// verify the page title
+	    String getCourse= wd.findElement(By.xpath("//div[@class='ribbon-widget-wrapper-split ribbon-widget-wrapper']/div/div/div/div[@id='courseName']")).getText();
+	    Assert.assertEquals(getCourse,CourseName);
+	    
+	}
+	
+	@Given("^User is on the course page$")
+	public void user_is_on_the_course_page()
+	{
+		
+	}
+	
+	@When("^User selects any unit$")
+	public void user_selects_any_unit()
+	{
+		
+	}
+	
+	@Then("^ All the chapters should appear in content$")
+	public void all_the_chapters_should_appear_in_content(String filePath,String fileName,String sheetName) throws IOException	
+	{
+		
+		File file =    new File(filePath+"\\"+fileName);
+
+	    //Create an object of FileInputStream class to read excel file
+
+	    FileInputStream inputStream = new FileInputStream(file);
+
+	    Workbook macWorkbook = null;
+
+	    //Find the file extension by splitting file name in substring  and getting only extension name
+
+	    String fileExtensionName = fileName.substring(fileName.indexOf("."));
+
+	    //Check condition if the file is xlsx file
+
+	   // if(fileExtensionName.equals(".xlsx")){
+
+	    //If it is xlsx file then create object of XSSFWorkbook class
+
+	//macWorkbook = new XSSFWorkbook(inputStream);
+
+	   // }
+
+	    //Check condition if the file is xls file
+
+	    if(fileExtensionName.equals(".xls")){
+
+	        //If it is xls file then create object of HSSFWorkbook class
+
+	        macWorkbook = new HSSFWorkbook(inputStream);
+
+	    }
+
+	    //Read sheet inside the workbook by its name
+
+	    Sheet macSheet = macWorkbook.getSheet(sheetName);
+
+	    //Find number of rows in excel file
+
+	    int rowCount = macSheet.getLastRowNum()-macSheet.getFirstRowNum();
+
+	    //Create a loop over all the rows of excel file to read it
+
+	    for (int i = 0; i < rowCount+1; i++) {
+
+	        Row row = macSheet.getRow(i);
+
+	        //Create a loop to print cell values in a row
+
+	        for (int j = 0; j < row.getLastCellNum(); j++) {
+
+	            //Print Excel data in console
+
+	            System.out.print(row.getCell(j).getStringCellValue()+"|| ");
+
+	        }
+
+	        System.out.println();
+		
+		
+	} }
+	
+	    
+	   @Given("^User has selected Resources tab$")
+	   public void user_has_selected_resources_tab()
+	   {
+		   
+		   
+	   }
+	   
+	   
+	   @When("^ User selects Content By Type$")
+	   public void user_selects_Content_By_Type()
+	   {
+	   }
+	   
+	   @Then("^All the resources should appear as in menuscript$")
+	   public void all_the_resources_should_appear_as_in_menuscript()
+	   {
+		   
+	   }
+	   
+	   
+				
+}
